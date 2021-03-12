8cfbdda1ad1852da3c9ad8a91289c91b620acffd
Jack L <jack.edward.lyons@gmail.com>
2021-03-12 11:04:11 +1000

    Feat: Add seed data and hook up Apollo on FE

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,23 @@
 import type { AppProps /* , AppContext */ } from 'next/app'
+import { ApolloProvider, ApolloClient } from '@apollo/client'
 import useRouteProgressBar from '../hooks/useRouteProgressBar'
 import Page from '../components/Page'
+import withData from '../lib/withData'
 import '../components/styles/nprogress.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+  apollo,
+}: AppProps & { apollo: ApolloClient<any> }) {
   useRouteProgressBar()
 
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   )
 }
 
@@ -17,12 +25,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 // every single page in your application. This disables the ability to
 // perform automatic static optimization, causing every page in your app to
 // be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-//
-//   return { ...appProps }
-// }
+MyApp.getInitialProps = async ({ Component, context }) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  if (Component.getInitialProps) {
+    const appProps = await Component.getInitialProps(context)
+    appProps.query = context.query
 
-export default MyApp
+    return { ...appProps }
+  }
+}
+
+export default withData(MyApp)
